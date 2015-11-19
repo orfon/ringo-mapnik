c6035be15342a81e28796d2e6df3c93b14e60f4d
Simon Oberhammer <simon.oberhammer@orf.at>
2015-11-19 14:25:50 +0100

    add ability to render a single tile

---
+++ external/render_tile.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+import mapnik
+import os
+import sys
+from math import pi,cos,sin,log,exp,atan
+from base64 import b64encode
+
+DEG_TO_RAD = pi/180
+RAD_TO_DEG = 180/pi
+
+class GoogleProjection:
+    def __init__(self,levels=18):
+        self.Bc = []
+        self.Cc = []
+        self.zc = []
+        self.Ac = []
+        c = 256
+        for d in range(0,levels):
+            e = c/2;
+            self.Bc.append(c/360.0)
+            self.Cc.append(c/(2 * pi))
+            self.zc.append((e,e))
+            self.Ac.append(c)
+            c *= 2
+
+    def fromLLtoPixel(self,ll,zoom):
+         d = self.zc[zoom]
+         e = round(d[0] + ll[0] * self.Bc[zoom])
+         f = minmax(sin(DEG_TO_RAD * ll[1]),-0.9999,0.9999)
+         g = round(d[1] + 0.5*log((1+f)/(1-f))*-self.Cc[zoom])
+         return (e,g)
+
+    def fromPixelToLL(self,px,zoom):
+         e = self.zc[zoom]
+         f = (px[0] - e[0])/self.Bc[zoom]
+         g = (px[1] - e[1])/-self.Cc[zoom]
+         h = RAD_TO_DEG * ( 2 * atan(exp(g)) - 0.5 * pi)
+         return (f,h)
+
+class ImageProvider:
+    def __init__(self, mapfile):
+        self.m = mapnik.Map(256, 256)
+        # Load style XML
+        mapnik.load_map(self.m, mapfile, True)
+        # Obtain <Map> projection
+        self.prj = mapnik.Projection(self.m.srs)
+        # Projects between tile pixel co-ordinates and LatLong (EPSG:4326)
+        self.tileproj = GoogleProjection(19)
+
+    def render_tile(self, x, y, z):
+
+        # Calculate pixel positions of bottom-left & top-right
+        p0 = (x * 256, (y + 1) * 256)
+        p1 = ((x + 1) * 256, y * 256)
+
+        # Convert to LatLong (EPSG:4326)
+        l0 = self.tileproj.fromPixelToLL(p0, z);
+        l1 = self.tileproj.fromPixelToLL(p1, z);
+
+        # Convert to map projection (e.g. mercator co-ords EPSG:900913)
+        c0 = self.prj.forward(mapnik.Coord(l0[0],l0[1]))
+        c1 = self.prj.forward(mapnik.Coord(l1[0],l1[1]))
+
+        # Bounding box for the tile
+        if hasattr(mapnik,'mapnik_version') and mapnik.mapnik_version() >= 800:
+            bbox = mapnik.Box2d(c0.x,c0.y, c1.x,c1.y)
+        else:
+            bbox = mapnik.Envelope(c0.x,c0.y, c1.x,c1.y)
+        render_size = 256
+        self.m.resize(render_size, render_size)
+        self.m.zoom_to_box(bbox)
+        if(self.m.buffer_size < 128):
+            self.m.buffer_size = 128
+
+        # Render image with default Agg renderer
+        im = mapnik.Image(render_size, render_size)
+        mapnik.render(self.m, im)
+        return im.tostring('png');
+        #return b64encode(im.tostring('png'))
+
+if __name__ == "__main__":
+
+    mapfile = os.environ['MAPNIK_MAP_FILE']
+    x = int(os.environ['MAPNIK_X'])
+    y = int(os.environ['MAPNIK_Y'])
+    z = int(os.environ['MAPNIK_Z'])
+
+    renderer = ImageProvider(mapfile);
+    imgString = renderer.render_tile(x, y, z);
+    sys.stdout.write(imgString);
