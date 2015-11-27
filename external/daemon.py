02cccfbaae96b5c39817af90c67d460c4ca5f8e0
Simon Oberhammer <simon.oberhammer@orf.at>
2015-11-27 13:29:23 +0100

    deal with errors

--- external/daemon.py
+++ external/daemon.py
@@ -8,14 +8,24 @@ while 1:
     line = sys.stdin.readline()
     if not line:
         break
-    env = json.loads(line)
-    renderer = ImageProvider(str(env.get('mapnikXml')))
-    imgString = renderer.render_tile(env.get('x'), env.get('y'), env.get('z'))
-    imgStringByteArray = bytearray(imgString)
-    imgStringLen = len(imgStringByteArray)
-    sys.stderr.write('len: ' + str(imgStringLen) + '\n')
-    sys.stderr.flush()
+    try:
+        env = json.loads(line)
+        renderer = ImageProvider(str(env.get('mapnikXml')))
+        imgString = renderer.render_tile(env.get('x'), env.get('y'), env.get('z'))
+        imgStringByteArray = bytearray(imgString)
+        imgStringLen = len(imgStringByteArray)
+        # for debugging
+        # sys.stderr.write('len: ' + str(imgStringLen) + '\n')
+        # sys.stderr.flush()
 
-    sys.stdout.write(struct.pack('!i', imgStringLen))
-    sys.stdout.write(imgStringByteArray);
-    sys.stdout.flush()
+        sys.stdout.write(struct.pack('!i', imgStringLen))
+        sys.stdout.write(imgStringByteArray)
+        sys.stdout.flush()
+    except Exception as e:
+        sys.stderr.write(str(e) + '\n')
+        sys.stderr.flush()
+        # write 0 so java nows there was an error
+        sys.stdout.write(struct.pack('!i', 0))
+        sys.stdout.flush()
+
+
