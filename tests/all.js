70e3d1aeae3eec2cafafbe41d560ee268f7e471d
Simon Oberhammer <simon.oberhammer@orf.at>
2016-01-29 11:43:19 +0100

    run tests for ogr and geojson plugin

--- tests/all.js
+++ tests/all.js
@@ -3,12 +3,17 @@ var assert = require('assert');
 var fs = require('fs');
 
 var viennaXmlPath = module.resolve('./fixtures/austrians-vienna.xml');
+var viennaOgrXmlPath = module.resolve('./fixtures/austrians-vienna-ogr.xml');
 var testOutputPng = module.resolve('./fixtures/test-output.png');
 
 exports.setUp = function() {
    var xml = fs.read(viennaXmlPath + '.template');
    xml = xml.replace('%%BASE_PATH%%', module.resolve('./fixtures/'));
    fs.write(viennaXmlPath, xml);
+
+   var xmlOgr = fs.read(viennaOgrXmlPath + '.template');
+   xml = xml.replace('%%BASE_PATH%%', module.resolve('./fixtures/'));
+   fs.write(viennaOgrXmlPath, xml);
 }
 
 exports.tearDown = function() {
@@ -20,40 +25,44 @@ exports.tearDown = function() {
    try {
       fs.remove(viennaXmlPath);
    } catch (e) {
-      // test might have failed
+      //
+   }
+   try {
+      fs.remove(viennaOgrXmlPath);
+   } catch (e) {
+      //
    }
 }
 
 exports.testDaemon = function() {
-   var renderDeamon = new RenderDeamon();
-   renderDeamon.start();
-   var imgBinary = renderDeamon.render({
-      mapnikXml: module.resolve('./fixtures/austrians-vienna.xml'),
-      x: 279,
-      y: 177,
-      z: 9
+   [viennaXmlPath, viennaOgrXmlPath].forEach(function(mapnikXml) {
+      var renderDeamon = new RenderDeamon();
+      renderDeamon.start();
+      var imgBinary = renderDeamon.render({
+         mapnikXml: mapnikXml,
+         x: 279,
+         y: 177,
+         z: 9
+      });
+      renderDeamon.stop();
+      var binaryImage = JSON.parse(fs.read(module.resolve('./fixtures/daemon-binary-image-result.json')));
+      assert.deepEqual(imgBinary.toArray(), binaryImage);
    });
-   renderDeamon.stop();
-   var binaryImage = JSON.parse(fs.read(module.resolve('./fixtures/daemon-binary-image-result.json')));
-   assert.deepEqual(imgBinary.toArray(), binaryImage);
-}
-
-exports.testTiles = function() {
-
 }
 
 exports.testBounding = function() {
 
-   var viennaBounding = [15.967787227555085,48.081620729494375,16.85708055141957,48.32764203530962];
-   renderBounding({
-      mapnikXml: viennaXmlPath,
-      boundingBox: viennaBounding,
-      tileFile: testOutputPng
+   [viennaXmlPath, viennaOgrXmlPath].forEach(function(mapnikXml) {
+      var viennaBounding = [15.967787227555085,48.081620729494375,16.85708055141957,48.32764203530962];
+      renderBounding({
+         mapnikXml: mapnikXml,
+         boundingBox: viennaBounding,
+         tileFile: testOutputPng
+      });
+
+      var referencePng = fs.read(module.resolve('./fixtures/reference-bounding-output.png'), {binary: true}).toArray();
+      var outputPng = fs.read(testOutputPng, {binary: true}).toArray();
+
+      assert.deepEqual(referencePng, outputPng);
    });
-
-
-   var referencePng = fs.read(module.resolve('./fixtures/reference-bounding-output.png'), {binary: true}).toArray();
-   var outputPng = fs.read(testOutputPng, {binary: true}).toArray();
-
-   assert.deepEqual(referencePng, outputPng);
 }
